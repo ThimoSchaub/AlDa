215e7df7d7941a9c0482a64372c9083ff6a61a80
ThimoSchaub <thimoschaub@web.de>
2018-01-02 16:28:29 +0100

    start

--- src/graph/AdjacencyListDirectedGraph.java
+++ src/graph/AdjacencyListDirectedGraph.java
@@ -3,82 +3,175 @@
 
 package graph;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+
 
 /**
- * Implementierung von DirectedGraph mit einer doppelten HashMap 
- * fÃ¼r die Nachfolgerknoten und einer einer doppelten HashMap 
+ * Implementierung von DirectedGraph mit einer doppelten HashMap
+ * fÃ¼r die Nachfolgerknoten und einer einer doppelten HashMap
  * fÃ¼r die VorgÃ¤ngerknoten.
  * <p>
- * Entspicht einer Adjazenzlisten-Implementierung 
+ * Entspicht einer Adjazenzlisten-Implementierung
  * mit schnellem Zugriff auf die Knoten.
+ *
+ * @param <V> Knotentyp.
  * @author Anton Hummel & Thimo Schaub
  * @since 02.01.2018
- * @param <V> Knotentyp.
  */
 public class AdjacencyListDirectedGraph<V> implements DirectedGraph<V> {
     // doppelte HashMap für die Nachfolgerknoten:
-    public final HashMap<V, HashMap<V, Double>> succ = new HashMap<>(); 
-    
+    public final HashMap<V, HashMap<V, Double>> succ = new HashMap<>();
+
     // doppelte HashMap für die VorgÃ¤ngerknoten:
-    public final HashMap<V, HashMap<V, Double>> pred = new HashMap<>(); 
-    
-    
+    public final HashMap<V, HashMap<V, Double>> pred = new HashMap<>();
+
+    private static int post = 1;
     private int numberEdge = 0;
 
-   
+    /**
+     * Gibt alle Knoten nach der Tiefensuche als Liste zurück.
+     * @param g Graph
+     * @param <V> Typ der Knoten
+     * @return gibt die Liste aller Knoten und die damit verbundenen Knoten zurück
+     */
+    public static <V> List<List<V>> depthFirstSearchAllNodes(DirectedGraph<V> g) {
+        List<List<V>> list = new LinkedList<>();
+        HashMap<V, Integer> besucht = new HashMap<V, Integer>();
+        for (V v : g.getVertexList()) {
+            if (!besucht.containsKey(v)) {
+                List<V> liste = new LinkedList<>();
+                list.add(depthFirstSearchAllNodes(v, g, besucht, liste));
+            }
+        }
+        return list;
+    }
+
+    /**
+     * Rekursive Funktion der Tiefensuche
+     * @param v Vertex
+     * @param g Graph
+     * @param besucht Hashmap für den besucht-Zustand
+     * @param list Liste der Knoten
+     * @param <V> Typ der Knoten
+     * @return gibt die Liste aller Knoten zurück die mit dem Startknoten v verbunden sind
+     */
+    private static <V> List<V> depthFirstSearchAllNodes(V v, DirectedGraph<V> g, HashMap besucht, List<V> list) {
+
+        if (!besucht.containsKey(v)) {
+            besucht.put(v, 0);
+            list.add(v);
+            for (V m : g.getSuccessorVertexList(v)) {
+                depthFirstSearchAllNodes(m, g, besucht, list);
+            }
+
+            besucht.put(v, post++);
+            }
+        return list;
+    }
+
+    /**
+     * Topologische Sortierung des Graphens
+     * @param g Graph
+     * @param <V> Typ der Knoten
+     * @return Liste der sortierten Knoten
+     */
+    public static <V> List<V> topologicalSort(DirectedGraph<V> g) {
+        List<V> ts = new LinkedList<>();
+        HashMap<V,Integer> inDegree =new HashMap<>() ;
+        Queue<V> q =new ArrayDeque<>();
+
+        for(V v : g.getVertexList()) {
+            inDegree.put(v,g.getInDegree(v));
+            if(g.getInDegree(v)==0){
+                q.add(v);
+            }
+        }
+        while(!q.isEmpty()) {
+            V v = q.remove();
+            ts.add(v);
+            for (V w : g.getSuccessorVertexList(v)) {
+                int i = inDegree.get(w)-1;
+                inDegree.put(w,i);
+                if (i==0){
+                    q.add(w);
+                }
+            }
+        }
+        if (ts.size()!= g.getNumberOfVertexes())
+            return null;
+        else
+            return ts;
+
+
+    }
+
+    /**
+     * Invertiert die KAnten um festzustellen welche Knoten Stong Komponents sind
+     * @param g graphh der durchsucht wird
+     * @param <V> typ der knoten
+     * @return Liste der Strong Komponents  und den damit verbundenen Knoten
+     */
+    public static <V>List<List<V>>findStrongComponents(DirectedGraph<V>g){
+        DirectedGraph<V>g2 = new AdjacencyListDirectedGraph<V>();
+        for (V v: g.getVertexList()){
+            g2.addVertex(v);
+        }
+        for(Edge e: g.getEdgeList()){
+            g2.addEdge((V)e.target,(V)e.source,e.weight);
+        }
+        return depthFirstSearchAllNodes(g2);
+    }
 
     @Override
     public boolean addVertex(V v) {
-    	if (!succ.containsKey(v)){
-    		succ.put(v, new HashMap<>());
-    		pred.put(v, new HashMap<>());
-    		return true;
-    	} else {
-        	return false;
-    	}
-    	
+        if (!succ.containsKey(v)) {
+            succ.put(v, new HashMap<>());
+            pred.put(v, new HashMap<>());
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     @Override
     public boolean addEdge(V v, V w) {
-    	if (!containsVertex(v)) {
-    		addVertex(v);
-    	}
-    	if (!containsVertex(v)) {
-    		addVertex(w);
-    	}
+        if (!containsVertex(v)) {
+            addVertex(v);
+        }
+        if (!containsVertex(v)) {
+            addVertex(w);
+        }
         if (containsEdge(v, w)) {
-        	succ.get(v).put(w, 1.0);
-        	pred.get(w).put(v, 1.0);
-        	return false;
-        }else {
-        	succ.get(v).put(w, 1.0);
-        	pred.get(w).put(v, 1.0);
-        	numberEdge++;
-        	return true;
-    	}
+            succ.get(v).put(w, 1.0);
+            pred.get(w).put(v, 1.0);
+            return false;
+        } else {
+            succ.get(v).put(w, 1.0);
+            pred.get(w).put(v, 1.0);
+            numberEdge++;
+            return true;
+        }
     }
+
     @Override
     public boolean addEdge(V v, V w, double weight) {
-    	if (!containsVertex(v)) {
-    		addVertex(v);
-    	}
-    	if (!containsVertex(w)) {
-    		addVertex(w);
-    	}
+        if (!containsVertex(v)) {
+            addVertex(v);
+        }
+        if (!containsVertex(w)) {
+            addVertex(w);
+        }
         if (containsEdge(v, w)) {
-        	succ.get(v).put(w, weight);
-        	pred.get(w).put(v, weight);
-        	return false;
-        }else {
-        	succ.get(v).put(w, weight);
-        	pred.get(w).put(v, weight);
-        	numberEdge++;
-        	return true;
-    	}
+            succ.get(v).put(w, weight);
+            pred.get(w).put(v, weight);
+            return false;
+        } else {
+            succ.get(v).put(w, weight);
+            pred.get(w).put(v, weight);
+            numberEdge++;
+            return true;
+        }
     }
 
     @Override
@@ -88,20 +181,20 @@ public class AdjacencyListDirectedGraph<V> implements DirectedGraph<V> {
 
     @Override
     public boolean containsEdge(V v, V w) {
-    	if (!containsVertex(v)||!containsVertex(w)) {
-    		throw new IllegalArgumentException(v +" or "+ w + " isn`t a Vertex");
-    	}
+        if (!containsVertex(v) || !containsVertex(w)) {
+            throw new IllegalArgumentException(v + " or " + w + " isn`t a Vertex");
+        }
         return succ.get(v).containsKey(w);
     }
 
     @Override
     public double getWeight(V v, V w) {
-    	 return succ.get(v).get(w);
+        return succ.get(v).get(w);
     }
 
     @Override
     public int getNumberOfVertexes() {
-    	return succ.size();
+        return succ.size();
     }
 
     @Override
@@ -113,76 +206,76 @@ public class AdjacencyListDirectedGraph<V> implements DirectedGraph<V> {
     public List<V> getVertexList() {
         LinkedList<V> list = new LinkedList<>();
         for (V v : succ.keySet()) {
-        	list.add(v);       	
+            list.add(v);
         }
         return list;
     }
 
     @Override
     public List<Edge<V>> getEdgeList() {
-    	 List<Edge<V>> liste= new LinkedList<>();
-         for (V m : getVertexList()) {
-        	 for (V n : succ.get(m).keySet()) {
-        		liste.add(new Edge<V>(m,n,getWeight(m,n)));
-        	 	 
-    		 }
-         }
-         return liste;
+        List<Edge<V>> liste = new LinkedList<>();
+        for (V m : getVertexList()) {
+            for (V n : succ.get(m).keySet()) {
+                liste.add(new Edge<V>(m, n, getWeight(m, n)));
+
+            }
+        }
+        return liste;
     }
-    
+
     @Override
     public int getInDegree(V v) {
-    	if (!containsVertex(v)) {
-    		throw new IllegalArgumentException("The Argument"+ v +" isn`t a Vertex");
-    	}
-    	int in = 0;
-    	List<Edge<V>> n = getEdgeList();
-    	for (Edge<V> e : n) {
-			if (e.getTarget()== v) {
-				in++;
-			}
-		}
-    	return in;
+        if (!containsVertex(v)) {
+            throw new IllegalArgumentException("The Argument" + v + " isn`t a Vertex");
+        }
+        int in = 0;
+        List<Edge<V>> n = getEdgeList();
+        for (Edge<V> e : n) {
+            if (e.getTarget() == v) {
+                in++;
+            }
+        }
+        return in;
     }
 
     @Override
     public int getOutDegree(V v) {
-    	if (!containsVertex(v)) {
-    		throw new IllegalArgumentException("The Argument "+ v +" isn`t a Vertex");
-    	}
-    	int out = 0;
-    	List<Edge<V>> n = getEdgeList();
-    	for (Edge<V> e : n) {
-			if (e.getSource()== v) {
-				out++;
-			}
-		}
-    	return out;
+        if (!containsVertex(v)) {
+            throw new IllegalArgumentException("The Argument " + v + " isn`t a Vertex");
+        }
+        int out = 0;
+        List<Edge<V>> n = getEdgeList();
+        for (Edge<V> e : n) {
+            if (e.getSource() == v) {
+                out++;
+            }
+        }
+        return out;
     }
 
     @Override
     public List<V> getPredecessorVertexList(V v) {
-    	if (!containsVertex(v)) {
-    		throw new IllegalArgumentException("The Argument "+ v +" isn`t a Vertex");
-    	}
-    	List<V> list = new LinkedList<>();
-    	for (V n : pred.get(v).keySet()) {
-    		list.add(n);
-    	 	 
-		 }
-    	return list;
+        if (!containsVertex(v)) {
+            throw new IllegalArgumentException("The Argument " + v + " isn`t a Vertex");
+        }
+        List<V> list = new LinkedList<>();
+        for (V n : pred.get(v).keySet()) {
+            list.add(n);
+
+        }
+        return list;
     }
 
     @Override
     public List<V> getSuccessorVertexList(V v) {
-    	if (!containsVertex(v)) {
-    		throw new IllegalArgumentException("The Argument "+ v +" isn`t a Vertex");
-    	}
-    	List<V> list = new LinkedList<>();
-    	for (V n : succ.get(v).keySet()) {
-    		list.add(n);
-    	 	 
-		 }
-    	return list;
+        if (!containsVertex(v)) {
+            throw new IllegalArgumentException("The Argument " + v + " isn`t a Vertex");
+        }
+        List<V> list = new LinkedList<>();
+        for (V n : succ.get(v).keySet()) {
+            list.add(n);
+
+        }
+        return list;
     }
 }
--- src/graph/main.java
+++ src/graph/main.java
@@ -1,21 +1,76 @@
 package graph;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class main {
 
+    public static AdjacencyListDirectedGraph<Integer> graph = new AdjacencyListDirectedGraph<Integer>();
+    public static AdjacencyListDirectedGraph<Integer> graph2 = new AdjacencyListDirectedGraph<Integer>();
+    public static AdjacencyListDirectedGraph<String> anziehen = new AdjacencyListDirectedGraph<String>();
 	public static void main(String[] args) {
 		int n = 7;
 		// Anzahl Knoten
-		AdjacencyListDirectedGraph<Integer> adjacencyList = new AdjacencyListDirectedGraph<Integer>();
-		System.out.println(adjacencyList.addVertex(5));
-		System.out.println(adjacencyList.addVertex(6));
-		System.out.println(adjacencyList.addVertex(4));
-		System.out.println(adjacencyList.succ);
-		System.out.println(adjacencyList.addEdge(4, 5 , 200.0));
-		//System.out.println(adjacencyList.addEdge(4, 5));
-		System.out.println(adjacencyList.addEdge(4, 6));
-		System.out.println(adjacencyList.addEdge(6, 5));
-		System.out.println(adjacencyList.getPredecessorVertexList(6));
-				
+
+		for (int i = 1; i <= 7; i++)
+		    graph.addVertex(i);
+        graph.addEdge(1,2);
+        graph.addEdge(2,5);
+        graph.addEdge(5,1);
+        graph.addEdge(2,6);
+        graph.addEdge(4,6);
+        graph.addEdge(4,3);
+        graph.addEdge(3,7);
+        graph.addEdge(7,4);
+        System.out.println(AdjacencyListDirectedGraph.findStrongComponents(graph));
+
+        for (int i = 1; i <= 10; i++)
+            graph2.addVertex(i);
+        graph2.addEdge(1,2);
+        graph2.addEdge(1,4);
+        graph2.addEdge(2,3);
+        graph2.addEdge(2,6);
+        graph2.addEdge(3,1);
+        graph2.addEdge(3,4);
+        graph2.addEdge(3,5);
+        graph2.addEdge(4,5);
+        graph2.addEdge(6,3);
+        graph2.addEdge(7,6);
+        graph2.addEdge(7,8);
+        graph2.addEdge(8,6);
+        graph2.addEdge(8,10);
+        graph2.addEdge(10,9);
+        graph2.addEdge(9,8);
+        //System.out.println(AdjacencyListDirectedGraph.depthFirstSearchAllNodes(graph2));
+
+        anziehen.addVertex("Socken");
+        anziehen.addVertex("Schuhe");
+        anziehen.addVertex("Unterhose");
+        anziehen.addVertex("Hose");
+        anziehen.addVertex("Gürtel");
+        anziehen.addVertex("Unterhemd");
+        anziehen.addVertex("Hemd");
+        anziehen.addVertex("Pulli");
+        anziehen.addVertex("Mantel");
+        anziehen.addVertex("Schal");
+        anziehen.addVertex("Handschuhe");
+        anziehen.addVertex("Mütze");
+        anziehen.addEdge("Schuhe", "Handschuhe");
+        anziehen.addEdge("Schal", "Handschuhe");
+        anziehen.addEdge("Mütze", "Handschuhe");
+        anziehen.addEdge("Socken", "Schuhe");
+        anziehen.addEdge("Hose", "Schuhe");
+        anziehen.addEdge("Hose", "Gürtel");
+        anziehen.addEdge("Unterhose", "Hose");
+        anziehen.addEdge("Unterhemd", "Hemd");
+        anziehen.addEdge("Hemd", "Pulli");
+        anziehen.addEdge("Pulli", "Mantel");
+        anziehen.addEdge("Mantel", "Schal");
+        anziehen.addEdge("Gürtel", "Mantel");
+        anziehen.addEdge("Schal", "Hose");
+        //System.out.println(AdjacencyListDirectedGraph.topologicalSort(anziehen));
+
+
 		/*for (int i = 0; i < n; i++)
 			adjacencyList.put((i, new AdjacencyListDirectedGraph();
 		adjacencyList.get(4).put(3, 43);
@@ -26,5 +81,6 @@ public class main {
 		// Kante von 4 nach 2 mit Gewicht 42 eintragen
 		*/
 	}
+
 
 }
